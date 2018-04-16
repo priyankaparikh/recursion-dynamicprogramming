d35fe8cda53c8dff226627a3137eb708bea811be
Priyanka Parikh <priyanka.parikh6@gmail.com>
2018-04-16 14:58:36 -0700

    fibonacci in python recursively

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,9 @@
 def fibo(n):
     """ accepts an in integer as input and returns
     the fibonacci number """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
